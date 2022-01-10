07b23b3238194364bac75be9330415650b884e57
Drishti <drishti.talreja.dt@gmail.com>
2022-01-10 22:10:04 +0530

    Subject choices remaining

--- vidyadisha/accounts/urls.py
+++ vidyadisha/accounts/urls.py
@@ -11,6 +11,9 @@ router.register('student', views.StudentViewSet, basename='student')
 router.register('tutor', views.TutorViewSet, basename='tutor')
 router.register('donor', views.DonorViewSet, basename='donor')
 router.register('mentor', views.MentorViewSet, basename='mentor')
+router.register('stu-requiring-mentor', views.StuRequiringMentorViewSet, basename='stu-requiring-mentor')
+router.register('stu-requiring-donor', views.StuRequiringDonorViewSet, basename='stu-requiring-donor')
+router.register('stu-requiring-tutor', views.StuRequiringTutorViewSet, basename='stu-requiring-tutor')
 """
 router.register('customuser', views.CustomUserViewSet, basename='customuser')
 router.register('student', views.StudentAdditionalViewSet, basename='student')
@@ -20,6 +23,11 @@ router.register('donor', views.DonorAdditionalViewSet, basename='donor')
 """
 urlpatterns = [
     path('', include(router.urls)),
+    path('user-register', views.RegisterUser.as_view(), name="user-register"),
+    path('student-register', views.RegisterStudent.as_view(), name="student-register"),
+    path('mentor-register', views.RegisterMentor.as_view(), name="mentor-register"),
+    path('donor-register', views.RegisterDonor.as_view(), name="donor-register"),
+    path('tutor-register', views.RegisterTutor.as_view(), name="tutor-register"),
     #path('user-list/',views.UserList,name="User"),
     #path('student-list/',views.StudentList,name="Student"),
     #path('stucreate/', views.create_student, name='stucreate'),
